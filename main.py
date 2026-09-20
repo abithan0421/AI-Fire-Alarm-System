b5232b5a7fb80190bdd66b7f5da3e0c9b559c54c
abithan0421 <abitha.n0421@gmail.com>
2026-09-20 21:10:20 +0530

    [feat]: Add fine tuned model

--- main.py
+++ main.py
@@ -14,25 +14,22 @@ SENDER_PASSWORD = email_config["SENDER_PASSWORD"]
 RECIPIENT_EMAIL = email_config["RECIPIENT_EMAIL"]
 
 pygame.mixer.init()
-pygame.mixer.music.load("audio.mp3") 
-
+pygame.mixer.music.load("audio.mp3")
 
 cap = cv2.VideoCapture(0)
 model = YOLO('fire.pt')
-
-
 classnames = ['fire']
-alarm_playing = False  
+alarm_playing = False
 
 def play_alarm():
     global alarm_playing
-    if not alarm_playing: 
-        pygame.mixer.music.play(-1)  
+    if not alarm_playing:
+        pygame.mixer.music.play(-1)
         alarm_playing = True
 
 def stop_alarm():
     global alarm_playing
-    if alarm_playing: 
+    if alarm_playing:
         pygame.mixer.music.stop()
         alarm_playing = False
 
@@ -40,53 +37,56 @@ def send_notification():
     print("Sending notification: Fire detected!")
     subject = "Fire Alert!"
     body = "Fire has been detected by the AI Fire Alarm system. Please take immediate action"
+    
     msg = EmailMessage()
     msg["Subject"] = subject
     msg["From"] = SENDER_EMAIL
     msg["To"] = RECIPIENT_EMAIL
     msg.set_content(body)
-
+    
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()  
+            server.starttls()
             server.login(SENDER_EMAIL, SENDER_PASSWORD)
             server.send_message(msg)
-
-        print("Notification email sent successfully!")
-
+            print("Notification email sent successfully!")
     except Exception as e:
         print(f"Failed to send notification. Error: {e}")
-
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+        
     frame = cv2.resize(frame, (640, 480))
     result = model(frame, stream=True)
-
-    fire_detected = False 
-
+    fire_detected = False
+    
     for info in result:
         boxes = info.boxes
         for box in boxes:
             confidence = math.ceil(box.conf[0] * 100)
             Class = int(box.cls[0])
-
+            
             if confidence > 50 and classnames[Class] == "fire":
                 fire_detected = True
-                break 
+ 
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+          
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                
+                label = f"Fire: {confidence}%"
+                cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
     if fire_detected:
         print("Fire detected!")
-        play_alarm() 
+        play_alarm()
         send_notification()
     else:
         print("No fire detected.")
-        stop_alarm()  
-
-   
+        stop_alarm()
+        
     cv2.imshow('Fire Detection', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
